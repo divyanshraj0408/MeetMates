c4aaf08261b45a1cac80f235882b057e4dfa394d
Sagar <sagarjha2004@gmail.com>
2025-03-27 09:47:11 +0530

    added google auth

---
+++ frontend/src/googleAuth.js
@@ -0,0 +1,17 @@
+// src/googleAuth.js
+export const renderGoogleButton = (elementId, clientId, callback) => {
+  window.google.accounts.id.initialize({
+    client_id: clientId,
+    callback: callback,
+  });
+
+  window.google.accounts.id.renderButton(
+    document.getElementById(elementId),
+    {
+      theme: "outline",
+      size: "large",
+      type: "standard",
+      width: 300,
+    }
+  );
+};
