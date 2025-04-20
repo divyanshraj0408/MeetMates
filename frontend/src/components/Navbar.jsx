c9c231d0828ca3923968c4e7436a1a2e9c8858c7
Divyansh <divyanshraj0408@gmail.com>
2025-04-20 22:11:45 +0530

    backbbutton beautified

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,17 @@ const Navbar = ({ onGoToLogin, goBack, doDisplay }) => {
   return (
     <>
       <header className="navbar">
-        <button
+        <div
           className="menu-icon"
           onClick={goBack}
           style={{ display: `${doDisplay}` }}
         >
-          {"<-"}
-        </button>
+          {
+            <span className="material-symbols-outlined">
+              arrow_back_ios_new
+            </span>
+          }
+        </div>
         <p className="logo">pingo</p>
         <button className="cta-btn" onClick={onGoToLogin}>
           Log In
